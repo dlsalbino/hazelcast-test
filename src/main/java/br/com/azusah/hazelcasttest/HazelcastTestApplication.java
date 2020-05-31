bb4f3ce1b6f5a5b4b895156984de5cfea3707ccc
Daniel Albino <daniel.albino@gmail.com>
2020-05-30 23:45:47 -0300

    starting with hazelcast.

---
+++ src/main/java/br/com/azusah/hazelcasttest/HazelcastTestApplication.java
@@ -0,0 +1,13 @@
+package br.com.azusah.hazelcasttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HazelcastTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HazelcastTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/azusah/hazelcasttest/HazelcastTestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.azusah.hazelcasttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HazelcastTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
